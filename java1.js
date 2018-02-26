36e3dbccb7409fdf2d3bdd06996a4c6b7aa6a531
chawlautsav <chawlautsav@gmail.com>
2018-02-26 17:54:39 +0530

    Jquery and icons added

--- java1.js
+++ java1.js
@@ -15,7 +15,13 @@
  	      newScript.innerHTML = document.getElementById("codeJavaScript").value;
  	       headID.appendChild(newScript); 
  	   } 
-
-
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
+$(document).ready(function(){
+    $("textarea").keyup(function(){
+        doIt();
+    });
+});
 
  	   function view(xsl) {var menu = document.getElementById('codeSelection'); var col3 = document.getElementById('col3'); var col9 = document.getElementById('col9'); if ( xsl == 1) {menu.setAttribute('class','nav nav-tabs bg-light justify-content-end '); col3.setAttribute('class','col-lg-3 order-2'); col9.setAttribute('class','col-lg-9 order-1'); } else if( xsl == 21) {menu.setAttribute('class','nav nav-tabs bg-light'); col3.setAttribute('class','col-lg-3 order-2'); col9.setAttribute('class','col-lg-9 order-12'); } }
