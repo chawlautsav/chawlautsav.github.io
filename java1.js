b34eda0e3b471c9bbfa0684eb5bcda89c1cb548d
chawlautsav <chawlautsav@gmail.com>
2018-03-01 19:13:36 +0530

    EDITOR CHANGED

--- java1.js
+++ java1.js
@@ -1,49 +1,19 @@
+
+var eSheetsHTML = ace.edit("editor-HTML");
+	eSheetsHTML.getSession().setMode("ace/mode/html");
+	document.getElementById('editor-HTML').style.fontSize='15px';
+	eSheetsHTML.session.setTabSize(3);
+var eSheetsCSS = ace.edit("editor-CSS");
+	eSheetsCSS.getSession().setMode("ace/mode/css");
+	document.getElementById('editor-CSS').style.fontSize='16px';
+	eSheetsCSS.session.setUseWrapMode(true);
+var eSheetsJS = ace.edit("editor-JS");
+	eSheetsJS.getSession().setMode("ace/mode/javascript");
+	eSheetsJS.session.setUseWrapMode(true);
+	document.getElementById('editor-JS').style.fontSize='14px';
+	eSheetsJS.session.setTabSize(2);
 $(document).ready(function()
 {
-	var code = $(".CM-html")[0];
-	var editor1 = CodeMirror.fromTextArea(code, {
-		lineNumbers : true,
-		mode:  "xml",
-		lineWrapping: true,
-		scrollbarStyle : "overlay"
-	} );
-	editor1.setSize("100%", 550);
-	var code1 = $(".CM-css")[0];
-	var editor2 = CodeMirror.fromTextArea(code1, {
-		lineNumbers : true,
-		mode : "css",
-		lineWrapping: true,
-		scrollbarStyle : "overlay"
-	} );
-	editor2.setSize("100%", 550);
-	var code2 = $(".CM-js")[0];
-	var editor3 = CodeMirror.fromTextArea(code2, {
-		lineNumbers : true,
-		mode: "javascript",
-		lineWrapping: true,
-		scrollbarStyle : "overlay"
-	} );
-
-	editor3.setSize("100%", 550);
-	function doIt()
-	{
-	 	var x = document.getElementById("output"); 
-	 	var y = (x.contentWindow || x.contentDocument);
-
-	 	if (y.document)y = y.document;
-	 	y.close(); 
-	 	y.write(editor1.getValue()); 
-	 	y.write('<head> </head>');
-		var headID = y.getElementsByTagName("head")[0];
-		var newStyle = document.createElement("style");
-		newStyle.type = "text/css";
-		newStyle.innerHTML = editor2.getValue();
-		headID.appendChild(newStyle); var headID = y.getElementsByTagName("head")[0]; 
-		var newScript = document.createElement("script"); 
-		newScript.type = "text/javascript";
-		newScript.innerHTML = editor3.getValue();
-		headID.appendChild(newScript); 
-	} 
     $('[data-toggle="popover"]').popover();   
     $("textarea").keyup(function(){
         doIt();
@@ -52,19 +22,19 @@ $(document).ready(function()
  function doIt() {
  	var x = document.getElementById("output"); 
  	var y = (x.contentWindow || x.contentDocument);
-
-	if (y.document)y = y.document; y.close(); 
-	y.write(document.getElementById("codeHTML").value); 
+ 	y.close();
+	if (y.document)y = y.document;
+	 y.close(); 
+	y.write(eSheetsHTML.getValue()); 
 	y.write('<head> </head>');
 	var headID = y.getElementsByTagName("head")[0];
 	var newStyle = document.createElement("style");
 	newStyle.type = "text/css";
-	newStyle.innerHTML = document.getElementById("codeCSS").value;
+	newStyle.innerHTML = eSheetsCSS.getValue();
 	headID.appendChild(newStyle); var headID = y.getElementsByTagName("head")[0]; 
 	var newScript = document.createElement("script"); 
 	newScript.type = "text/javascript";
-	newScript.innerHTML = document.getElementById("codeJavaScript").value;
+	newScript.innerHTML = eSheetsJS.getValue();
 	headID.appendChild(newScript); 
  } 
-
- 	   function view(xsl) {var menu = document.getElementById('codeSelection'); var col3 = document.getElementById('col3'); var col9 = document.getElementById('col9'); if ( xsl == 1) {menu.setAttribute('class','nav nav-tabs bg-light justify-content-end '); col3.setAttribute('class','col-lg-3 order-2'); col9.setAttribute('class','col-lg-9 order-1'); } else if( xsl == 21) {menu.setAttribute('class','nav nav-tabs bg-light'); col3.setAttribute('class','col-lg-3 order-2'); col9.setAttribute('class','col-lg-9 order-12'); } }
+function view(xsl) {var menu = document.getElementById('codeSelection'); var col3 = document.getElementById('col3'); var col9 = document.getElementById('col9'); if ( xsl == 1) {menu.setAttribute('class','nav nav-tabs bg-light justify-content-end '); col3.setAttribute('class','col-lg-3 order-2'); col9.setAttribute('class','col-lg-9 order-1'); } else if( xsl == 21) {menu.setAttribute('class','nav nav-tabs bg-light'); col3.setAttribute('class','col-lg-3 order-2'); col9.setAttribute('class','col-lg-9 order-12'); } }
